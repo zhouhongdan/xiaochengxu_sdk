097a69c9f1b2987c88d97ec6c93ef3e4a7318034
zhouhongdan <872230776@qq.com>
2019-07-02 17:15:15 +0800

    删除不需要的

--- pages/longtou/index.js
+++
@@ -1,6 +0,0 @@
-var e = require("../BasePage");
-
-Page(e("龙头股日报", {
-    title: "点击下方图片，免费领取",
-    imageUrl: "./assets/share.jpg"
-}));
--- pages/traders/index.js
+++
@@ -1,6 +0,0 @@
-var e = require("../BasePage");
-
-Page(e("华蒋操盘室", {
-    title: "点击下方图片，免费领取建仓股池",
-    imageUrl: "./assets/share.jpg"
-}));
